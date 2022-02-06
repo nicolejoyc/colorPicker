6709a61a9d38f531a35bde51608e880f8402c140
Nicole Crumbaker <ncrumbaker@student.cvtc.edu>
2022-02-05 19:36:45 -0600

    [finishes #18116282] Implemented conversion to pybl through js

---
+++ scripts/colorPicker.js
@@ -0,0 +1,50 @@
+// Add event listener for when the form (.rbgConverter)
+// is submitted
+document.querySelector(".rgbConverter").addEventListener("submit", function(e) {
+
+  // This keeps the page from refreshing since
+  // I don't actually need to send form data
+  e.preventDefault();
+
+  var rgbRed = Number(document.querySelector("#redRGB").value);
+  var rgbGreen = Number(document.querySelector("#greenRGB").value);
+  var rgbBlue = Number(document.querySelector("#blueRGB").value);
+
+  function validateRGB() {
+
+    // long if statement to check if any of the 
+    // RGB inputs are less than 0 or greater than 1
+    if (rgbRed < 0 || rgbRed > 1 || rgbGreen < 0 || rgbGreen > 1 || rgbBlue < 0 || rgbBlue > 1) {
+
+      return false;
+
+    } else { 
+
+      return true;
+
+    }
+  }
+
+  if (validateRGB()) {
+    const LUMINANCE = 1.0;
+
+    var pyblResult = document.querySelector("#newPYBL");
+
+    var pyblRed, pyblGreen, pyblBlue = 0;
+    var rgbTotal = rgbRed + rgbGreen + rgbBlue;
+
+    // Convert rgb values to pybl values
+    pyblRed = (rgbRed / rgbTotal).toFixed(2);
+    pyblGreen = (rgbGreen / rgbTotal).toFixed(2);
+    pyblBlue = (rgbBlue / rgbTotal).toFixed(2);
+
+    //console.log(pyblRed + " " + pyblGreen + " " + pyblBlue);
+
+    // Edit square result box to display pybl values
+    pyblResult.innerHTML = "&rho;&gamma;&beta;L(" + pyblRed + ", " + pyblGreen + ", " + pyblBlue + ", " + LUMINANCE + ")";
+
+  } else { // one of the rgb inputs is not valid
+    alert("Please enter values between 0 and 1.");
+  }
+
+})
